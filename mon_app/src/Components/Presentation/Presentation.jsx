61b112232ab7e47fa29d1dfb58739ad17011a669
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-09-18 17:24:52 +0200

    presentation

--- mon_app/src/Components/Presentation/Presentation.jsx
+++ mon_app/src/Components/Presentation/Presentation.jsx
@@ -13,8 +13,14 @@ function presentation() {
            <img className="presentation_image" src='/profile.webp' alt="Photo_de_profil" fetchPriority="high" />
            <article>
             <p className='text_presentation'>
-            Salut, je m'appelle <strong className='nom'>Adonis</strong>.
+            Salut je m'appelle <strong>Adonis,</strong> <br />
+            J’ai commencé à coder avec <strong>freeCodeCamp</strong>, puis j’ai approfondi 
+            mes connaissances grâce aux <strong>cours en libre accès d’OpenClassrooms</strong>. 
+            Au fil de mes apprentissages, j’ai pris goût au développement web et 
+            j’ai décidé d’en faire une nouvelle orientation professionnelle. <br />
 
+            <br /> <strong>Fort de 10 ans d’expérience dans le domaine de l’informatique</strong>, 
+            je me reconvertis aujourd’hui dans le développement web. 
             Ayant suivi la <strong> formation Intégrateur Web chez OpenClassrooms</strong>, je 
             mets à profit mon expertise technique, ma rigueur et mon expérience 
             pour concevoir des interfaces <strong>modernes, responsives et accessibles</strong>. 
