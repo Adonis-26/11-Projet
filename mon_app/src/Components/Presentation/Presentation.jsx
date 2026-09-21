d0e6c3833d617d029746abdc763ef34d321f0a3c
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2026-09-22 01:02:30 +0200

    mise  à jour cv

--- mon_app/src/Components/Presentation/Presentation.jsx
+++ mon_app/src/Components/Presentation/Presentation.jsx
@@ -30,7 +30,7 @@ function presentation() {
             <a id="contact_presentation" href="#contact">
             contact
             </a>
-            <a className="cv_download" href="/adonis-YN-DEV.pdf.pdf" download>
+            <a className="cv_download" href="/adonis-YN-DEV.pdf" download>
             Télécharger le CV
             </a>
          
